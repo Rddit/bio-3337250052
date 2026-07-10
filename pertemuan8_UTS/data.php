225d67bc7afdf2825aff96d492408baa4d58b095
Radit <radityapone@gmail.com>
2026-07-10 08:39:28 +0700

    change: index.html to index.php + add data.php

--- pertemuan8_UTS/data.php
+++ pertemuan8_UTS/data.php
@@ -1,5 +1,14 @@
 <?php
 $profil = [
     "nama" => "Raditya Ahmad Pratama",
-    
+    "kampus" => "Universitas Sultan Ageng Tirtayasa",
+    "nim" => "3337250052",
+    "bio" => "Mahasiswa Informatika berpengalaman kerja & magang di jaringan. Termotivasi untuk terus mengembangkan keterampilan, memperluas ilmu dan pengalaman di bidang IT",
+    "hobby" => "Workout",
+    "skills" => [
+        'Networking' => 'Berpengalaman dalam konfigurasi jaringan lokal (LAN), manajemen router/switch Cisco, serta instalasi infrastruktur telekomunikasi.',
+        "Python" => 'Menguasai Python dasar untuk otomatisasi skrip tugas, pengolahan data menggunakan pustaka Pandas, dan dasar kecerdasan buatan.',
+        "C++" => 'Memahami konsep pemrograman berorientasi objek (OOP), struktur data kompleks, dan algoritma menggunakan bahasa C++.',
+        "HTML" => 'Mampu menyusun struktur halaman web yang semantik, responsif menggunakan Bootstrap, serta ramah SEO.'
+    ]
 ];
--- pertemuan8_UTS/index.php
+++ pertemuan8_UTS/index.php
@@ -1,62 +1,81 @@
-<?php include 'data.php'?>
+<?php include 'data.php' ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title><?= $profil['nama'] ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <script src="https://kit.fontawesome.com/957301ad34.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <header>
-      <div class="header-content">
-        <h1>Raditya Ahmad Pratama</h1>
-        <button id="btnToggle" class="btn btn-dark"><i class="bi bi-brightness-high-fill"></i></button>
-        <p class="tagline">Informatics Student - UNTIRTA</p>
-      </div>
-    </header>
 
-    <main>
-      <div class="content-wrapper">
+<head>
+  <meta charset="UTF-8" />
+  <title><?= $profil['nama'] ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <script src="https://kit.fontawesome.com/957301ad34.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css" />
+  <script src="script.js" defer></script>
+</head>
+
+<body>
+  <header>
+    <div class="header-content">
+      <h1><?= $profil['nama'] ?></h1>
+      <button id="btnToggle" class="btn btn-dark"><i class="bi bi-brightness-high-fill"></i></button>
+      <p class="tagline"><?= $profil['kampus']; ?></p>
+    </div>
+  </header>
+
+  <main>
+    <div class="content-wrapper">
       <section id="tentang">
         <h2>About Me</h2>
-        <p>NIM: 3337250052</p>
-        <p>My Hobby is Workout</p>
+        <p>NIM: <?= $profil['nim'] ?></p>
+        <p><?= $profil['bio'] ?></p>
+        <p>Hobi: <?= $profil['hobby'] ?></p>
       </section>
 
       <section id="skill">
         <h2>Skill</h2>
-        <div class="skill-list">
-          <span class="skill-item">Networking</span>
-          <span class="skill-item">Python</span>
-          <span class="skill-item">C++</span>
-          <span class="skill-item">HTML</span>
+        <div class="skill-list" id="skillAccordion">
+
+          <! Skill List >
+          <div class="d-inline-flex gap-1">
+            <?php $i = 0;
+            foreach ($profil['skills'] as $nama_skill => $penjelasan): ?>
+              <a data-bs-toggle="collapse" href="#collapse-<?= $i; ?>" role="button" aria-expanded="false" aria-controls="collapse-<?= $i; ?>">
+                <span class="skill-item"><?= $nama_skill; ?></span>
+              </a>
+            <?php $i++;
+            endforeach; ?>
+          </div>
+
+          <! Penjelasan tiap Skill>
+          <?php $j = 0;
+          foreach ($profil['skills'] as $nama_skill => $penjelasan): ?>
+            <div class="collapse <?= ($j == 0) ? 'show' : '' ?>" id="collapse-<?= $j; ?>" data-bs-parent="#skillAccordion">
+              <div class="skill-desc mt-3">
+                <?= $penjelasan; ?>
+              </div>
+            </div>
+          <?php $j++;
+          endforeach; ?>
         </div>
       </section>
-      </div>
-      <section id="fakta">
-        <h2><i class="bi bi-router"></i> Fact of the Day</h2>
-        <p id="isi-fakta">Genarating Fact...</p>
-        <button id="btnRefresh" class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-arrow-clockwise"></i></button>
-      </section>
+    </div>
 
-    </main>
-  
-    <footer>
-      <a href="mailto:radityapone@gmail.com"
-        ><button class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-envelope-at-fill"></i></button>
-      </a>
-      <a href="https://instagram.com/rddit.ya" target="_blank"
-        ><button class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-instagram"></i></button>
-      </a>
-      <a href="https://github.com/Rddit" target="_blank"
-        ><button class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-github"></i></button>
-      </a>
-    </footer>
-    <script src="script.js"></script>
-  </body>
+    <section id="fakta">
+      <h2><i class="bi bi-router"></i> Fact of the Day</h2>
+      <p id="isi-fakta">Genarating Fact...</p>
+      <button id="btnRefresh" class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-arrow-clockwise"></i></button>
+    </section>
+  </main>
+
+  <footer>
+    <a href="mailto:radityapone@gmail.com"><button class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-envelope-at-fill"></i></button>
+    </a>
+    <a href="https://instagram.com/rddit.ya" target="_blank"><button class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-instagram"></i></button>
+    </a>
+    <a href="https://github.com/Rddit" target="_blank"><button class="btn btn-dark" style="font-size: 1.5rem; color: rgb(255, 141, 251);"><i class="bi bi-github"></i></button>
+    </a>
+  </footer>
+</body>
+
 </html>
