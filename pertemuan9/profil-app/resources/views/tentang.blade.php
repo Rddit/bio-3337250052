cc174a0936e1b690af3047de2252cd0cd00f3978
Radit <radityapone@gmail.com>
2026-07-15 11:22:47 +0700

    add: link to new repository for laravel project

--- pertemuan9/profil-app/resources/views/tentang.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Tentang Mata Kuliah')
-@section('content')
-    <h1>{{ $info['matakuliah'] }}</h1>
-    <p>{{ $info['semester'] }}</p>
-    <p>{{ $info['dosen'] }}</p>
-@endsection
---
+++ pertemuan9/readme.md
@@ -0,0 +1,2 @@
+tugas untuk pertemuan 9 dapat diakses pada repository berikut:
+https://github.com/Rddit/profil-app
